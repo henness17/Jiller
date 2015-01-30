afba19879e7dad31e0820e8e546d1d213afaee41
Ryan Henness <ryanhenness@gmail.com>
2015-01-29 16:19:08 -0800

    Added HTML view of stream data

--- Software Engineering/JLRdata/stream.py
+++ Software Engineering/JLRdata/stream.py
@@ -2,6 +2,8 @@ from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 import time
+import os
+from sys import exit
 
 ckey = 'lqg1QpaKYl81Xb6qgUhTREfPh'
 csecret = 'LGiMUtXhbQ0ie3ZDzamvuar2rkD8DXasUrYxXgZf1Z9KLMk74m'
@@ -11,24 +13,24 @@ asecret	= 'HremoM5vZ94wtAtH0QDDLd34H5KiNPKDwqvUMQRz39eD7'
 #Creating a listener class
 class listener(StreamListener):
 
-	#Collecting the data
+	#Collecting the on_data
 	def on_data(self, data):
-		try:
-			## MAY WANT TO SPLIT MORE DATA THAN JUST TWEET IN THE FUTURE, LIKE USERNAME AND LOCATION
-			tweet = data.split(',"text":"')[1].split('","source')[0] #Split the data into just the tweet
-			#location = data.split
-			#username = data.split
-
-			saveThis = str(time.time())+':::'+tweet #Get the time, add the tweet, NEED to add location
-
-			saveFile = open('tweetDB.csv', 'a') #Save file with intention to append
-			saveFile.write(saveThis) #Write incoming data to the file
-			saveFile.write('\n') #New line to separate the data
-			saveFile.close() #Close tweetDB
-			return True
-		except BaseException, e:
-			print 'failed ondata',str(e)
-			time.sleep(5) #Just in case you did get limited error
+			###try:
+				os.remove('tweetDB.html')
+				## MAY WANT TO SPLIT MORE DATA THAN JUST TWEET IN THE FUTURE, LIKE USERNAME AND LOCATION
+				tweet = data.split(',"text":"')[1].split('","source')[0] #Split the data into just the tweet
+				saveThis = str(time.time())+'::'+tweet #Get the time, add the tweet, NEED to add location
+				saveFile = open('tweetDB.html', 'a') #Save file with intention to append
+				saveFile.write("<li>"+tweet+"</li>") #Write incoming data to the file
+				saveFile.write('\n') #New line to separate the data
+				saveFile.close() #Close tweetDB
+				os.startfile('tweetDB.html')
+				time.sleep(2)
+				raise SystemExit
+				return True
+			##except BaseException, e:
+				print 'failed ondata',str(e)
+				time.sleep(5) #Just in case you did get limited error
 
 	def on_error(self, status):
 		print status #Print the status
@@ -39,5 +41,5 @@ auth.set_access_token(atoken, asecret)
 twitterStream = Stream(auth, listener())
 
 #Filtering the data that we want to show up
-twitterStream.filter(track=["car"]) 
+twitterStream.filter(track=["good morning"]) 
 #twitterStream.filter(locations=[-122.75,36.8,-121.75,37.8])
